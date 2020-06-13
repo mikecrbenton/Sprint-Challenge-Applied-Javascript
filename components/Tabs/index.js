ca7322ff0245edc1c5e61cbfaa69a87a15cdf31b
mikecrbenton <mikecrbenton@hotmail.com>
2020-06-13 12:03:26 -0500

    step 2 complete

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,23 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+   .then( (response) => {
+      response.data.topics.forEach( (header) => {
+
+         //CREATE ELEMENT, AND CONTENT
+         let listItem = document.createElement('div');
+         listItem.classList.add('tab');
+         let newText = document.createTextNode( header );
+
+         //APPEND 
+         listItem.textContent = header;
+
+         //ADD TO DOM
+         document.querySelector('.topics').appendChild(listItem);
+         console.log( document.querySelector('.topics'))
+      })
+   })
+   .catch( (error)  => console.log(error) );
